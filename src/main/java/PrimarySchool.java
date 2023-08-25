4deb021ea13e78013c6bea022d35b25f4b3876e7
pleewson <adrianplewait@gmail.com>
2023-08-25 20:24:49 +0200

    added 1st formula

--- src/main/java/PrimarySchool.java
+++ src/main/java/PrimarySchool.java
@@ -1,8 +1,34 @@
 public class PrimarySchool {
 
-    private int a;
+   /* private int a;
     private int b;
     private int c;
     private int h;
 
+    private double a;
+    private double b;
+    private double c;
+    private double h;
+
+    */
+
+     private double result;
+
+    public void triangleArea(double a, double h){
+        printFormulas("Triangle Area","1/2 a * h");
+        this.result = 1/2 * a * h;
+       // printResult("1/2" + " " + h );
+    }
+
+
+
+
+
+    public static void printFormulas(String formulasName, String formula){
+        System.out.println("Formulas - " + formulasName + '\n' + formula);
+    }
+
+    public static void printResult(String formulaData, double result){
+        System.out.println(formulaData + result);
+    }
 }
