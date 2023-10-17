f2afc089af9a5187b4fddcdcc473a5235c94cd4c
pleewson <adrianplewait@gmail.com>
2023-10-17 10:55:55 +0200

    added COLORS that contains ANSI colors

--- src/main/java/Areas.java
+++ src/main/java/Areas.java
@@ -19,56 +19,56 @@ public class Areas {
           //formulas
     public static void squareArea() {
         System.out.println("l - length of side");
-        System.out.println("A = l^2");
+        System.out.println(COLOR.YELLOW_TEXT + "A = l^2" + COLOR.RESET);
     }
 
     public static void rectangleArea() {
         System.out.println("w - width");
         System.out.println("h - height");
-        System.out.println("A = w * h");
+        System.out.println(COLOR.YELLOW_TEXT + "A = w * h" + COLOR.RESET);
     }
 
     public static void triangleArea() {
         System.out.println("b - base");
         System.out.println("h - height");
-        System.out.println("A = (b*h)/2");
+        System.out.println(COLOR.YELLOW_TEXT + "A = (b*h)/2" + COLOR.RESET);
     }
 
     public static void rhombusArea() {
         System.out.println("D - large digitional");
         System.out.println("d - small digitional");
-        System.out.println("A = (D * d)/2");
+        System.out.println(COLOR.YELLOW_TEXT + "A = (D * d)/2" + COLOR.RESET);
     }
 
     public static void trapezoidArea(){
         System.out.println("B - large side");
         System.out.println("b - small side");
         System.out.println("h - height");
-        System.out.println("A = (B + b)/2 * h");
+        System.out.println(COLOR.YELLOW_TEXT + "A = (B + b)/2 * h" + COLOR.RESET);
     }
 
     public static void regularPolygonArea(){
         System.out.println("P - perimeter");
         System.out.println("a - apothem");
-        System.out.println("A = (P/2) * a");
+        System.out.println(COLOR.YELLOW_TEXT + "A = (P/2) * a" + COLOR.RESET);
     }
 
     public static void circleArea(){
         System.out.println("r - radius");
         System.out.println("P - perimeter");
         System.out.println("A = πr^2");
-        System.out.println("P = 2πr");
+        System.out.println(COLOR.YELLOW_TEXT + "P = 2πr" + COLOR.RESET);
     }
 
     public static void coneArea(){
         System.out.println("r - radius");
         System.out.println("s - slant height");
-        System.out.println("A = πr * s");
+        System.out.println(COLOR.YELLOW_TEXT + "A = πr * s" + COLOR.RESET);
     }
 
     public static void sphereArea(){
         System.out.println("r - radius");
-        System.out.println("A = 4πr^2");
+        System.out.println(COLOR.YELLOW_TEXT + "A = 4πr^2" + COLOR.RESET);
     }
 
 
@@ -81,7 +81,7 @@ public class Areas {
             int numberOfFormnula = scan.nextInt();
 
             if (numberOfFormnula > formulaList.size() || numberOfFormnula < 1) {
-                System.out.println("Invalid number, try again.");
+                System.out.println(COLOR.RED_TEXT + "Invalid number, try again." + COLOR.RESET);
             } else {
 
                 switch (numberOfFormnula) {
---
+++ src/main/java/COLOR.java
@@ -0,0 +1,19 @@
+public class COLOR {
+    
+
+
+            public static String RESET = "\u001B[0m";   //reset
+
+            public static String RED_TEXT = "\u001B[31m";
+            public static String GREEN_TEXT = "\u001B[32m";
+            public static String YELLOW_TEXT = "\u001B[33m";
+
+
+
+            public static String BLACK_BG = "\u001B[40m";
+
+
+
+        
+    }
+
